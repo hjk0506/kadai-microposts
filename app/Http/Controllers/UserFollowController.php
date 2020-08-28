46cfea30b69a9b0268069dc0797776d39aed0150
kenichi.hajake <hajake@astro-b.jp>
2020-08-28 14:33:17 +0000

    timeline

--- app/Http/Controllers/UserFollowController.php
+++ app/Http/Controllers/UserFollowController.php
@@ -33,4 +33,16 @@ class UserFollowController extends Controller
         // 前のURLへリダイレクトさせる
         return back();
     }
+        /**
+     * このユーザとフォロー中ユーザの投稿に絞り込む。
+     */
+    public function feed_microposts()
+    {
+        // このユーザがフォロー中のユーザのidを取得して配列にする
+        $userIds = $this->followings()->pluck('users.id')->toArray();
+        // このユーザのidもその配列に追加
+        $userIds[] = $this->id;
+        // それらのユーザが所有する投稿に絞り込む
+        return Micropost::whereIn('user_id', $userIds);
+    }
 }
