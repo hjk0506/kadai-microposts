d46156969de316155aa4d89a83fc15df44f7fe7b
kenichi.hajake <hajake@astro-b.jp>
2020-08-28 16:23:39 +0000

    test1

--- routes/web.php
+++ routes/web.php
@@ -36,6 +36,12 @@ Route::group(['middleware' => ['auth']], function () {
     });
 
     Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+    
+    // 追加
+    Route::group(['prefix' => 'microposts/{id}'], function () {
+        Route::post('favorite', 'FavoritesController@store')->name('favorites.favorite');
+        Route::delete('unfavorite', 'FavoritesController@destroy')->name('favorites.unfavorite');
+    });
 
     Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
 });
